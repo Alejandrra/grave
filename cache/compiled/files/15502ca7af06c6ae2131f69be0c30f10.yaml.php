979bf69fe5f8ca29ffe3558e55a50bf7b3cd6976
root <root@d72244a00531>
2019-06-04 17:11:09 +0000

    first commit

---
+++ cache/compiled/files/15502ca7af06c6ae2131f69be0c30f10.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/grav/user/accounts/admin.yaml',
+    'modified' => 1559605962,
+    'data' => [
+        'state' => 'enabled',
+        'email' => 'nicole73711@gmail.com',
+        'fullname' => 'Alejandra Montenegro',
+        'title' => 'Administrador',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$JvoPsJbecZhV38Srhd9/re7/nMRlNopMwIqL23FFxURy9zFfR7TRu'
+    ]
+];
